44954aa09919e4f4de5e58f467babd9442f5f1ab
gullabi <bkulebi@gmail.com>
2020-05-14 09:02:05 +0000

    backup is tarred

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,3 +1,4 @@
-mongoexport -d suportmutu -c ar -o suportmutu_ar_`date +"%Y%m%d"`.json
-mongoexport -d suportmutu -c ur -o suportmutu_ur_`date +"%Y%m%d"`.json
-mongoexport -d suportmutu -c zh-CN -o suportmutu_zh_`date +"%Y%m%d"`.json
+mongoexport -d suportmutu -c ar -o suportmutu_ar.json
+mongoexport -d suportmutu -c ur -o suportmutu_ur.json
+mongoexport -d suportmutu -c zh-CN -o suportmutu_zh.json
+tar czf backup_`date +"%Y%m%d"`.tar.gz suportmutu*.json
